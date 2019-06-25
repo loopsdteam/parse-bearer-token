c7b817d6bb662d47b61a37869b7fb5f38ada2046
fkkmemi <fkkmemi@gmail.com>
2019-06-25 10:44:50 +0900

    string error exception added

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ module.exports = (req) => {
   if (!req.headers.authorization) return null
   const as = req.headers.authorization.split(' ')
   if (as.length !== 2) return null
+  if (typeof as[0] !== 'string') return null
+  if (typeof as[1] !== 'string') return null
   if (as[0].toLowerCase() !== 'bearer') return null
   if (as[1].length < 20) return null
   return as[1]
